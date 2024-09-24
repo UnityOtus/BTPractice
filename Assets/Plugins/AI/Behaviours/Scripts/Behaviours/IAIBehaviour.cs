f05ee2cd3f5e37352ebd286e76805205c0486d5e
starkre <gulkin.igor.developer@gmail.com>
2024-09-24 18:57:04 +0600

    Start!

--- Assets/Plugins/AI/Behaviours/Scripts/Behaviours/IAIBehaviour.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.Scripting.APIUpdating;
-
-namespace Atomic.AI
-{
-    [MovedFrom(true, "Modules.AI", "Modules.AI.BehaviourSet", "IBehaviour")] 
-    public interface IAIBehaviour
-    {
-    }
-}
--- Assets/Plugins/AI/Behaviours/Scripts/Util/UpdateMode.cs
+++
@@ -1,9 +0,0 @@
-namespace Atomic.AI
-{
-    internal enum UpdateMode
-    {
-        UPDATE = 0,
-        FIXED_UPDATE = 1,
-        MANUAL = 2,
-    }
-}
