ec3bace9670e4162918e13a47620e8bf61444851
Evgenii <justmysimplemail@gmail.com>
2024-09-24 23:23:44 +0500

    Move resources to barn

---
+++ Assets/Scripts/Engine/IsStorageFullCondition.cs
@@ -0,0 +1,16 @@
+﻿using Atomic.AI;
+using Game;
+using Game.Engine;
+
+namespace Engine
+{
+    public class IsStorageFullCondition : IBlackboardCondition
+    {
+        public bool Invoke(IBlackboard blackboard)
+        {
+            var character = blackboard.GetCharacter();
+            var storage = character.GetComponent<ResourceStorageComponent>();
+            return storage.IsFull();
+        }
+    }
+}
